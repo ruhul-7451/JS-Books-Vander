2f7b4ebadc25a6f2788210e2c9b6ca4a0e4149c5
ruhul-7451 <csehastakenmylife@gmail.com>
2021-09-02 17:12:49 +0600

    check done!

--- js/index.js
+++ js/index.js
@@ -2,30 +2,53 @@ const searchEngine = () => {
     const searchBox = document.getElementById('search-box');
     const searchText = searchBox.value;
     searchBox.value = '';
-    // console.log(searchText);
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
-
     fetch(url)
         .then(res => res.json())
         .then(data => loadBooks(data))
 }
 
 const loadBooks = (data) => {
-    // console.log(data);
+    console.log(data);
     const bookList = data.docs
+    /* Search Result Number */
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+
+    /* Search Field Validation */
+    const searchDiv = document.createElement('h6')
+    const nulSearchDiv = document.createElement('h6')
+    const noSearchResult = document.createElement('h6')
+    searchDiv.innerHTML = `<h6 class="text-center my-3">Search Result: ${data.num_found}</h6>`
+    nulSearchDiv.innerHTML = `<h6 class="text-center my-3">Please enter a book name</h6>`
+    noSearchResult.innerHTML = `<h6 class="text-center my-3">No Results Found</h6>`
+    if (data.q === '') {
+        searchResult.appendChild(nulSearchDiv);
+    }
+    else if (data.num_found === 0) {
+        searchResult.appendChild(noSearchResult);
+    }
+    else {
+        /* Show Search Result Count */
+        searchResult.appendChild(searchDiv);
+    }
+
+    /* Show Books in Grid */
+    const gridBooks = document.getElementById('grid-books');
+    gridBooks.textContent = '';
     bookList.forEach(book => {
-        console.log(book.title);
-        const gridBooks = document.getElementById('grid-books');
+        console.log(book);
         const newDiv = document.createElement('div')
         newDiv.classList.add('col')
-        newDiv.innerHTML = `<div class="card h-100">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${book.title}</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                additional content. This content is a little bit longer.</p>
-        </div>
-    </div>`
+        newDiv.innerHTML = `<div class="card h-100 shadow">
+    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top w-100 h-75 p-3">
+    <div class="card-body">
+        <h3 class="card-title">${book.title}</h3>
+        <p class="card-text"> <span class="fw-bold">Auhtor Name: </span>${book.author_name}</p>\n
+        <p class="card-text"><span class="fw-bold">Publisher: </span>${book.publisher}</p>\n
+        <p class="card-text"><span class="fw-bold">First Publish Year: </span>${book.first_publish_year}</p>            
+    </div>
+</div>`
         gridBooks.appendChild(newDiv);
     });
 
